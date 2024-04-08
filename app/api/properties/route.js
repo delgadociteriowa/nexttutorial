46c841e71064093673f9ccae919711a91243960f
Carlos <delgadociteriowa@gmail.com>
2024-04-08 17:34:50 -0300

    properties/route.js error

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,12 +1,15 @@
 import connectDB from '@/config/database';
+import Property from '@/models/Property';
 
+// GET /api/properties
 export const GET = async (request) => {
   try {
     await connectDB();
 
-    return new Response(JSON.stringify({ message: 'Hello World' }), {
-      status: 200,
-    });
+    // I have deleted .next folder, run everything again with the following new implementation, but still throws error
+    const properties = await Property.find({});
+
+    return Response.json(properties);
   } catch (error) {
     console.log(error);
     return new Response('Something went wrong', { status: 500 });
